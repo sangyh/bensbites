560f5f0e8ee570ba1b5ca48c09575a314715b137
Sangy <sangyh89@gmail.com>
2022-12-19 16:53:44 -0800

    formatting changes

--- frontend/src/components/SearchHome.vue
+++ frontend/src/components/SearchHome.vue
@@ -1,16 +1,20 @@
 <template>
+    <head>
+      <link rel="icon" type="image/x-icon" href="/favicon.ico">
+      <title>BensBites Tracker</title>
+    </head>
     <!-- Jumbotron -->
-    <div class="relative bg-gray-50 w-full text-center"> 
-        <div class="flex items-center justify-center py-10">
+    <div class="relative bg-gray-50 w-full text-center mb-4"> 
+        <div class="flex items-center justify-center py-5">
             <img
             src="~@/assets/bensbites_logo.jpg"
             alt="Bens bites logo"
             />
         </div>
         <div class="mx-16">
-            <h1 class="text-3xl font-bold mt-0 mb-6">Ben's Bites Lookup</h1>
-            <h3 class="text-1xl font-bold mb-8"> Semantic search to help find your favorite AI tools</h3>
-            <p class="text-gray-700 mb-8 leading-relaxed text-l ">
+            <h1 class="text-3xl font-bold mt-0 mb-2">Ben's Bites Tracker</h1>
+            <h3 class="text-1xl font-bold mb-0.5"> Find your favorite AI tools from <a class="text-red-500" href="https://bensbites.beehiiv.com/" target="_blank">Ben's Bites</a></h3>
+            <p class="text-gray-700 leading-relaxed text-l ">
                 Built by 
                   <a href="https://twitter.com/sangyh2" class="text-blue-500 underline">@sangyh2</a>
             </p>
@@ -18,40 +22,42 @@
     </div>
     <!-- Jumbotron -->
 
-    <form @submit.prevent="search" class="flex justify-center items-center py-10">
+    <form @submit.prevent="search" class="flex justify-center items-center py-2">
       <input type="text" v-model="query" placeholder="Search..." class="w-2/5 py-2 px-3 rounded-md bg-gray-200 placeholder-gray-700"/>
       <button type="submit"  class="py-2 px-3 rounded-md bg-blue-500 text-white">Go</button>
     </form>
     
-    <div v-if="isLoading" class="bg-gray-200 h-12 w-12 rounded-full mx-aut0">
-      <p>LOADING</p>
+    <div v-if="isLoading" class="w-full text-center px-20 py-4">
+      <p class="bg-gray-200 rounded-full">LOADING</p>
     </div>
     
     <div v-else>
       <div class="bg-gray-100 mx-auto mt-5 px-4 py-4" v-if="results.length > 0">
         <div class="flex justify-between items-center mb-4 font-semibold text-gray-800">
-            <div class="w-1/5">Post</div>
-            <div class="w-1/5">Section</div>
-            <div class="w-1/5">Info</div>
-            <div class="w-1/5">Link</div>
-            <div class="w-1/5">Similarity</div>
+            <div class="w-4/12">Post</div>
+            <div class="w-2/12">Section</div>
+            <div class="w-4/12">Info</div>
+            <div class="w-1/12">Link</div>
+            <div class="w-1/12">Similarity</div>
         </div>
 
         <table class="table-fixed w-full">
           <tbody>
-            <tr v-for="row in results" :key="row">
+            <tr v-for="row in results" :key="row" class="overflow-x-auto">
               <!-- <td v-for="value in row" v-bind:key="value"  class="justify-between items-center mb-4 w-1/5">{{ value }}</td> -->
-              <td class="justify-between items-center mb-4 w-1/5">{{ row.url }}</td>
-              <td class="justify-between items-center mb-4 w-1/5">{{ row.section }}</td>
-              <td class="justify-between items-center mb-4 w-1/5">{{ row.item_text }}</td>
-              <td class="justify-between items-center mb-4 w-1/5">
+              <td class="justify-between items-center mb-4 w-4/12 text-blue-500 underline">
+                <a v-bind:href="row.url" target="_blank">{{ row.url }}</a>
+              </td>
+              <td class="justify-between items-center mb-4 w-2/12">{{ row.section }}</td>
+              <td class="justify-between items-center mb-4 w-4/12">{{ row.item_text }}</td>
+              <td class="justify-between items-center mb-4 w-1/12">
                 <ul>
                   <li  v-for="ext_url in row.item_url" :key="ext_url">
                      <p class="text-blue-500 underline" v-html=formattedSentence(ext_url)></p>
                   </li>
                 </ul>
               </td>
-              <td class="justify-between items-center mb-4 w-1/5">{{ row.similarity }}</td>
+              <td class="justify-between items-center mb-4 w-1/12">{{ row.similarity }}</td>
             </tr>
           </tbody>
         </table> 
@@ -105,7 +111,7 @@ export default {
       const text = 'link' /* row[0] */
       const url = row[1]
       console.log(row[0])
-      const newtext = `<a href="${url}">${text}</a>`;
+      const newtext = `<a href="${url}" target="_blank">${text}</a>`;
       return newtext
       /* const sentence = row.item_text
       console.log('inside computed:', row)
@@ -120,3 +126,12 @@ export default {
 
 </script>
 
+<style>
+  /* Add the overflow-x-auto class to the table row */
+  tr.overflow-x-auto {
+    /* Use the 'table-row' display value to ensure that the table row maintains its normal layout */
+    display: table-row;
+    /* Apply the horizontal scrolling */
+    overflow-x: auto;
+  }
+</style>
